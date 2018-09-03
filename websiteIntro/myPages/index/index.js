c965ecf983af162f3b3e71381bcfb06c111abcc5
changxianglin <281532612@qq.com>
2018-09-04 01:14:35 +0800

    create new page

---
+++ websiteIntro/myPages/index/index.js
@@ -0,0 +1,84 @@
+// myPages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    FE: '前端',
+    BE: '后端',
+    OP: '运维'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+  toFE: function() {
+    wx.navigateTo({
+      url: '../list/list',
+    })
+  },
+  toBE: function() {
+    wx.navigateTo({
+      url: '../next/next',
+    })
+  },
+  toOP: function() {
+    wx.navigateTo({
+      url: '../support/support',
+    })
+  }
+})
